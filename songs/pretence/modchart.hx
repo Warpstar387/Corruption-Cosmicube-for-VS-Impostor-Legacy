eb066e8cf8615e07fa5f72770a42dc2e691eaf5c
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2026-07-22 17:53:02 -0300

    Add onCreatePost function to modchart.hx

---
+++ songs/pretence/modchart.hx
@@ -0,0 +1,7 @@
+function onCreatePost()
+{
+  modManager.setValue('alpha', 1, 1);
+  modManager.setValue('alpha', 0.6, 0);
+  modManager.setValue('drunk', 0.6, 0);
+  modManager.setValue('tipsy', 0.6, 0);
+}
