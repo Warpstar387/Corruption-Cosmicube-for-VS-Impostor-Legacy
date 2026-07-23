905b5473a36b940c2dce0f1a617f0f87976e9c6e
Warpstar387 <52362034+Warpstar387@users.noreply.github.com>
2026-07-23 18:29:48 +0100

    Delete songs/pretence/modchart.hx
    not wanting pretence to have a modchart just wanting it to be normal

--- songs/pretence/modchart.hx
+++
@@ -1,7 +0,0 @@
-function onCreatePost()
-{
-  modManager.setValue('alpha', 1, 1);
-  modManager.setValue('alpha', 0.6, 0);
-  modManager.setValue('drunk', 0.6, 0);
-  modManager.setValue('tipsy', 0.6, 0);
-}
